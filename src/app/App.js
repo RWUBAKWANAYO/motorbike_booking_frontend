780d75544a1b4711fe5a0283b9c565387ee3b0cc
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-10-04 09:40:34 +0000

    Fix linter errors

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
-
 function App() {
   return (
-    <div className="App"> 
-    </div>
+    <div className="App" />
   );
 }
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
+/* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
 
-
 export const store = configureStore({
-  reducer: {
-    
-  },
+  reducer: {},
 });
