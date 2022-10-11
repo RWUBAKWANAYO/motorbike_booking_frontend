1a1f0d50b1dd7cf81ecb83ab42ec660a24f9745c
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-10-11 18:11:25 +0000

    Fix route errors

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import Motorcycles from '../features/motorcycles/motorslist/MotorsList';
+import MotorDetails from '../features/motorcycles/motordetails/MotorDetails';
+import NewMotor from '../features/motorcycles/newmotor/NewMotor';
+
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" />
-        <Route exact path="/motorcycles" />
-        <Route exact path="/motorcycles/:motor_id/details" />
-        <Route exact path="/add_motorcycle" />
+        <Route exact path="/motorcycles" element={<Motorcycles />} />
+        <Route exact path="/motorcycles/:motor_id/details" element={<MotorDetails />} />
+        <Route exact path="/add_motorcycle" element={<NewMotor />} />
         <Route exact path="/delete_motorcycle" />
         <Route exact path="/my_reservations" />
         <Route exact path="/reserve/:motor_id" />
