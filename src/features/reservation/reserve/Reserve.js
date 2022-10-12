c3a96b9e94c9de58cef30b6d3623d0b70af3ec7f
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-10-12 08:53:24 +0000

    upate reserve UI

--- src/features/reservation/reserve/Reserve.js
+++ src/features/reservation/reserve/Reserve.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import motor1 from '../../../assets/images/motor1.png';
 import Sidebar from '../../../common/sidebar/Sidebar';
 import './Reserve.css';
+import ReserveForm from './ReserveForm';
 
-const Reserve = () => (
-  <div className="list-all-cont">
-    <Sidebar />
-    <div className="list-all-wrapper">
-      <FontAwesomeIcon icon={faBars} />
-      <div>
+const Reserve = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  return (
+    <div className="reserve-cont">
+      {showSidebar && (
+      <div className="reserve-sidebar">
+        <Sidebar />
+        <i
+          onClick={() => setShowSidebar(false)}
+          aria-hidden="true"
+        >
+          <FontAwesomeIcon icon={faXmark} />
+        </i>
+      </div>
+      )}
+
+      <div className="reserve-bg">
+        <img src={motor1} alt="motorcycle" className="reserve-bg" />
+        <div />
+      </div>
+
+      <div className="reserve-wrapper">
+        { !showSidebar && (
+        <i
+          className="reserve-menu"
+          onClick={() => setShowSidebar(true)}
+          aria-hidden="true"
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </i>
+        )}
         <h1>BOOK A VERSPA C-20 MOTORCYCLE</h1>
         <p>
           Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
@@ -19,17 +47,10 @@ const Reserve = () => (
           Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
           consectetur, adipisci velit
         </p>
-
-        <form>
-          <select name="City">
-            {Array.from(Array(10).keys()).map((el) => <option key={el} value="London">London</option>)}
-          </select>
-          <input type="date" id="birthday" name="birthday" />
-          <input type="submit" value="Book" />
-        </form>
+        <ReserveForm />
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Reserve;
