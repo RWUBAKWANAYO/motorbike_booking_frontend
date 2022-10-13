df32b238246766d94dbba74155d8789e765d4e7b
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-10-13 18:18:23 +0200

    set up signup reducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
+import signupSlice from '../features/authenticate/signup/signupSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    signup: signupSlice,
+  },
+
 });
