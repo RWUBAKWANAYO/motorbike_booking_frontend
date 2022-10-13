2f34c815281c4141e5e31fdcd0d4cec4959dbd81
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-10-13 20:20:18 +0200

    add dynamic data to motorcycle card

--- src/index.js
+++ src/index.js
@@ -11,11 +11,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
-  </React.StrictMode>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
 );
