7e11d26afb33e0679b684f02c0a535b84be8569a
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-10-13 19:48:56 +0200

    configure motorllist reducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -4,12 +4,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import signupSlice from '../features/authenticate/signup/signupSlice';
 import signinSlice from '../features/authenticate/signin/signinSlice';
 import authenticateSlice from '../features/authenticate/authenticateSlice';
+import motorslistSlice from '../features/motorcycles/motorslist/motorslistSlice';
 
 export const store = configureStore({
   reducer: {
     authforms: authenticateSlice,
     signup: signupSlice,
     signin: signinSlice,
+    motorslist: motorslistSlice,
   },
 
 });
